ffac7b014b4c8de76e375afda45a01cb18b854d8
kyle butcher <kylembutcher97@gmail.com>
2024-06-09 00:07:19 -0500

    finished for now. going to style little bit more in a bit

--- Assets/script.js
+++ Assets/script.js
@@ -2,28 +2,14 @@
 // cont lon=
 // const key=
 // let cityname = []
-const APIKEY = "716081892881e2753cc3fba701bb8257"
+const APIKEY = "2f045213741d82e002d3d0f2d5970b0f"
 const searchBtnEl = document.getElementById("search-button");
+const city = document.getElementById("searchCity");
 
 let citiesName = JSON.parse(localStorage.getItem("cityNames")) || [];
 
-
-// getting button working below 
-function getInfo (event){
-  event.preventDefault();
-  const searchInputEl = document.getElementById("search-info");
-  let searchTerm = searchInputEl.value;
-  const geoCoderURL = `http://api.openweathermap.org/geo/1.0/direct?q=${searchTerm}&limit=5&appid=${APIKEY}`
-  
-  fetch(geoCoderURL)
-  .then(data => data.json())
-  .then(geoCodeData => {
-    console.log(geoCodeData);
-  })
-};
-
 function getAPI() {
-    const requestURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`;
+    const requestURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city.value}&appid=${APIKEY}&units=imperial`;
     console.log(requestURL);
     fetch (requestURL)
       .then(response => response.json())
@@ -31,15 +17,15 @@ function getAPI() {
         console.log(data)
         deleteExtras()
         citiesName.push(data.city.name)
-        localStorage.setItem("citiesName", JSON.stringify(citiesName))
+        localStorage.setItem("cityName", JSON.stringify(citiesName))
         saveToLocalStorage(data)
-        populateInfo();
+        populateWeatherData();
       })
 };
   
   function deleteExtras() {
-    cityName = cityName.filter( (name, i) => !cityName.includes(name, i+1))
-    localStorage.setItem("cityName", JSON.stringify(cityNames))
+    cityName = cities.filter( (name, i) => !cities.includes(name, i + 1))
+    localStorage.setItem("cities", JSON.stringify(cityName))
     console.log(cityName)
   };
 
@@ -67,22 +53,22 @@ function getAPI() {
 
     const todaysDate = document.createElement("p");
     todaysDate.textContent = dayjs.unix(weather.list[0].dt).format("MM/DD/YYYY");
-    today.appendChild(todaysDate);
+    todaysWeather.appendChild(todaysDate);
 
     const windSpeed = document.createElement("p");
-    temp.textContent = `Wind ${weather.list[0].main.temp}`;
-    today.appendChild(windSpeed);
+    windSpeed.textContent = `Wind ${weather.list[0].wind.speed}mph`;
+    todaysWeather.appendChild(windSpeed);
 
     const humidity = document.createElement("p");
-    humidity.textContent = `Humidity: ${weather.list[i].main.humidity}%`;
-    today.appendChild(humidity);
+    humidity.textContent = `Humidity: ${weather.list[0].main.humidity}%`;
+    todaysWeather.appendChild(humidity);
 
     const today = document.createElement("p");
-    today.classList.add("weatherCards");
+    todaysWeather.classList.add("weatherCards");
 
     const temperature = document.createElement("p");
-    temperature.textContent = `Temp: ${weather.list[0].main.temp}`;
-    today.appendChild(temperature);
+    temperature.textContent = `Temp: ${weather.list[0].main.temp}°`;
+    todaysWeather.appendChild(temperature);
 
 
   // get the 5 day forecast here
@@ -90,35 +76,59 @@ function getAPI() {
 
   for (let i = 0; i < weather.list.length; i += 8){
     const forecast = document.querySelector("weekly-forecast");
-    forecast.classList.add("weatherCards");
+    weeklyForecast.classList.add("weatherCards");
 
+    const weeklyCard = document.createElement("div");
+    weeklyCard.classList.add("days");
     const date = document.createElement("p");
     date.textContent = dayjs.unix(weather.list[i].dt + 86400).format("MM/DD/YYYY");
-    forecast.appendChild(date);
+    weeklyCard.appendChild(date);
 
     const wind = document.createElement("p");
-    wind.textContent = `Wind: ${weather.lsit[i].wind.speed} mph`;
-    forecast.appendChild(wind);
+    wind.textContent = `Wind: ${weather.list[i].wind.speed} mph`;
+    weeklyCard.appendChild(wind);
 
     const humidity = document.createElement("p");
     humidity.textContent = `Humidity: ${weather.list[i].main.humidity}%`;
-    forecast.appendChild(humidity);
+    weeklyCard.appendChild(humidity);
 
     const temperature = document.createElement("p");
     temperature.textContent = `Temp: ${weather.list[i].main.temp}`;
-    forecast.appendChild(temperature);
-
+    weeklyCard.appendChild(temperature);
+    weeklyForecast.appendChild(weeklyCard);
   }
 };
-  
-  
+
+function displayCity(cityName) {
+  const displayedCity = document.querySelectorAll('h2');
+  displayedCity.forEach((h2) => {
+    h2.textContent = cityName;
+  });
+}
+
+// const cities = citiesName;
+// console.log(cities);
+// displayCity(cities);
+// deleteExtras()
+// document.querySelectorAll('.today').forEach(element => element.innerHTML ="");
+// document.querySelectorAll('.forecast').forEach(element => element.innerHTML="");
+
+searchBtnEl.addEventListener("click", function(e) {
+  e.preventDefault();
+  getAPI();
+  }
+);
+
+function deleteExtras() {
+
+}
   
   // getAPI();
   
-  searchBtnEl.addEventListener("click", getInfo);
+  // searchBtnEl.addEventListener("click", getAPI);
   
   // function getAPI2() {
-    //   const requestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&appid=${key}`;
-    //   console.log(requestURL);
-    // }
+  //     const requestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&appid=${key}`;
+  //     console.log(requestURL);
+  
     
